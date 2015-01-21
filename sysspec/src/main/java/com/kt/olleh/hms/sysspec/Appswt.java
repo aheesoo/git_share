29211dcb6074ccf77d8e8ab0bc4c4280a0ba8a71
aheesoo <aheesoo@gmail.com>
2015-01-21 21:10:15 +0900

    a

--- sysspec/src/main/java/com/kt/olleh/hms/sysspec/Appswt.java
+++ sysspec/src/main/java/com/kt/olleh/hms/sysspec/Appswt.java
@@ -202,7 +202,13 @@ public class Appswt
 				else if (fieldType == Integer.class)
 				{
 					Integer value = bytesToInteger(extractedData);
-					reValue = value.toString();
+
+					if("Command".equals(name)){
+						
+						reValue = byteArrayToHex(extractedData);
+					}else{
+						reValue = value.toString();
+					}
 					System.out.println(" int value : "+value);
 				}
 				else if (fieldType == byte[].class)
@@ -279,6 +285,21 @@ public class Appswt
 		}
 		return length;
 	}
+	
+	public static String byteArrayToHex(byte[] ba) {
+	    if (ba == null || ba.length == 0) {
+	        return null;
+	    }
+	 
+	    StringBuffer sb = new StringBuffer(ba.length * 2);
+	    String hexNumber;
+	    for (int x = 0; x < ba.length; x++) {
+	        hexNumber = "0" + Integer.toHexString(0xff & ba[x]);
+	 
+	        sb.append(hexNumber.substring(hexNumber.length() - 2));
+	    }
+	    return sb.toString();
+	} 
     
     public static void main(String[] args) {
     	new Appswt();
